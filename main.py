d2dab123011752b22a4b59182eec50b0b7eea349
luizmarr <luizhenriquemarreira08@gmail.com>
2026-05-18 15:49:44 -0300

    criando a minha main e adicionando um gitignore

---
+++ main.py
@@ -0,0 +1,15 @@
+from services.mapas import Mapas
+def main():
+    mapas = Mapas()
+    endereco = input("Digite um endereço: ")
+    resultado = mapas.geocode(endereco)
+
+    if resultado:
+        print(f"Endereço: {resultado['endereco']}")
+        print(f"Latitude: {resultado['latitude']}")
+        print(f"Longitude: {resultado['longitude']}")
+        print(f"País: {resultado['pais']}")
+    else:
+        print("Endereço não encontrado.")
+if __name__ == "__main__":    
+    main()
