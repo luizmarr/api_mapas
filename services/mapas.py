c5aef3379fcbd2160732b1e57fa9cdf493c381a6
luizmarr <luizhenriquemarreira08@gmail.com>
2026-05-18 13:48:47 -0300

    criando minha primeira classe

---
+++ services/mapas.py
@@ -0,0 +1,26 @@
+from geopy.geocoders import Nominatim
+
+class Mapas:
+    def __init__(self):
+        self.geolocator = Nominatim(user_agent="myGeocoder")
+
+    def geocode(self, endereco):
+        location = self.geolocator.geocode(endereco)
+        if location:
+            reverse_location = self.geolocator.reverse(
+                (location.latitude, location.longitude),
+                language="pt"
+            )
+
+            pais = reverse_location.raw.get("address", {}).get("country")
+
+            return {
+                "endereco": location.address,
+                "latitude": location.latitude,
+                "longitude": location.longitude,
+                "pais": pais
+            }
+        else:
+            return None 
+        
+
